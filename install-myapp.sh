84aa19db84f2cc5c20bada6227cd8deb7db9a621
AquaRiva <hmk0676@naver.com>
2025-07-09 09:35:18 +0900

    caption and 1 file delete

--- install-myapp.sh
+++ install-myapp.sh
@@ -5,7 +5,7 @@ helm repo add argo https://argoproj.github.io/argo-helm
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
-helm install argocd argo/argo-cd -n argocd --set server.extraArgs="{--insecure}" --set server.insecure=true
+helm install argocd argo/argo-cd -n argocd --set server.extraArgs="{--insecure}" --set server.insecure=true     # Only HTTP Health Check
 helm install prometheus prometheus-community/prometheus -n monitoring --create-namespace
 helm install grafana grafana/grafana -n monitoring
 helm install ingress ingress
